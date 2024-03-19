5133da8b821c1839b0ac152214b2bffd5162e48b
nithiskumar1211 <nithishkumarn248@gmail.com>
2024-03-19 12:42:50 +0530

    Day1

---
+++ Day1/src/pages/navigationData.js
@@ -0,0 +1,34 @@
+export const navigation = {
+  categories: [
+    {
+      id: "home",
+      name: "HOME",
+      link: "",
+    },
+    {
+      id: "services",
+      name: "SERVICE",
+      link: "services",
+    },
+    {
+      id: "tc",
+      name: "TERMS AND CONDITIONS",
+      link: "T&C",
+    },
+    // {
+    //   id: "faq",
+    //   name: "FAQ",
+    //   link: "faq",
+    // },
+    {
+      id: "Privacy Policy",
+      name: "PRIVACY POLICY",
+      link: "Policy",
+    },
+    // {
+    //   id: "About Us",
+    //   name: "About Us",
+    //   link: "about",
+    // },
+  ],
+};
