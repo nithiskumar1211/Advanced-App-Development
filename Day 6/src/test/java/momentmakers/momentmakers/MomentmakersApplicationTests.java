ba1138abcb549cec38b7dbbcefae1e6d441b3528
nithiskumar1211 <nithishkumarn248@gmail.com>
2024-03-26 12:19:12 +0530

    Day11

---
+++ Day 6/src/test/java/momentmakers/momentmakers/MomentmakersApplicationTests.java
@@ -0,0 +1,13 @@
+package momentmakers.momentmakers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomentmakersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Day 7/src/main/java/momentmakers/momentmakers/MomentmakersApplication.java
@@ -0,0 +1,13 @@
+package momentmakers.momentmakers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomentmakersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MomentmakersApplication.class, args);
+	}
+
+}
